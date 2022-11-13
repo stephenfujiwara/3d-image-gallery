7e2998ef5e2f61865fa8c5fd4d3b72089e242720
stephenfujiwara <stephenfujiwara456@gmail.com>
2022-11-13 02:52:00 -0800

    add frame component and place into gallery

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import { useRef, Suspense } from 'react'
 import { Canvas } from "@react-three/fiber"
-import { FirstPersonControls} from "@react-three/drei"
+import { FirstPersonControls, OrbitControls, FlyControls} from "@react-three/drei"
 
 import "./styles.css"
 
 import Text from "./components/Text"
 import Prism from "./components/Prism"
 import Stars from "./components/Stars"
+import Frame from "./components/Frame"
 
 export default function App() {
-  const ref = useRef()
-  
-  function stopMoving() {
-    ref.current.autoForward = !ref.current.autoForward
-    ref.current.activeLook = !ref.current.activeLook
-    console.log(ref.current.autoForward)
-  }
-
   return (
     <div className="canvas-container">
-      <Canvas onClick={stopMoving}>
-        <color attach="background" args={[0, 0, 0]}/>
+      <Canvas camera={{position : [0, 0, 6]}}>
+        <color attach="background" args={[255, 255, 255]}/>
         <Suspense fallback={null}>
-          <FirstPersonControls ref={ref} autoForward={true} movementForward={50} lookSpeed={0.15} heightMax={0} heightCoef={0}/>
+          <FirstPersonControls movementSpeed={0} lookVertical={false} lookSpeed={0.05}/>
           <Stars/>
           <Text/>
-          <Prism position={[0, -5, 0]} />
+          <Prism position={[0, -1, 0]} rotation-y={Math.PI / 4} scale={0.5} />
+          <Frame position={[3, 0, 4]} rotation={[0, -Math.PI / 2.5, 0]} url={"./tohji-t-mix-cover-art.jpg"}/>
         </Suspense>
       </Canvas>
     </div>
--- src/components/Text.js
+++ src/components/Text.js
@@ -5,7 +5,12 @@ export default function Text() {
     return (
         <React.Suspense fallback={null}>
             <Center center>
-                <Text3D letterSpacing={0.05} font={"/Inter_Medium_Regular.json"}>
+                <Text3D
+                bevelEnabled
+                bevelSize={0.04}
+                bevelThickness={0.1}
+                letterSpacing={0.05}
+                font={"/Inter_Medium_Regular.json"}>
                     <meshNormalMaterial/>
                     Hello World
                 </Text3D>
