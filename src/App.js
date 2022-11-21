41af05c604c3b5e9d8ff3834a91703e0f19d5649
stephenfujiwara <stephenfujiwara456@gmail.com>
2022-11-21 01:08:36 -0800

    show non-three js sidebar when clicking on image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState, useRef, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import { FirstPersonControls } from "@react-three/drei";
 
@@ -7,13 +7,25 @@ import "./styles.css";
 import Stars from "./components/Stars";
 import Frames from "./components/Frames";
 import Intro from "./components/Intro";
+import Sidebar from "./components/Sidebar";
 import { images } from "./components/images";
 
-// in frame: look at route with params, if id === url, then render component on the side with links, youtube/spotify
-
 export default function App() {
+  const [sidebar, setSidebar] = useState({
+    artist: "",
+    album: false,
+    title: "",
+  });
+
   return (
-    <div className="canvas-container">
+    <>
+      {sidebar?.artist && (
+        <Sidebar
+          artist={sidebar.artist}
+          album={sidebar.album}
+          title={sidebar.title}
+        />
+      )}
       <Canvas camera={{ position: [0, 0, 6] }}>
         <color attach="background" args={[0, 0, 0]} />
         <Suspense fallback={null}>
@@ -21,7 +33,7 @@ export default function App() {
           <Stars />
           {/*<Prism position={[0, -1, 0]} rotation-y={Math.PI / 4} scale={0.5} />
           <Text />*/}
-          <Frames images={images} />
+          <Frames images={images} setSidebar={setSidebar} />
           <FirstPersonControls
             makeDefault
             lookVertical={false}
@@ -32,6 +44,6 @@ export default function App() {
           <Intro />
         </Suspense>
       </Canvas>
-    </div>
+    </>
   );
 }
